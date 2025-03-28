8b9f225c06b89938f031fd14eb3c8fb94b953616
DelPalacioCarlos <carlosdelpalacio5@gmail.com>
2025-03-27 18:39:02 -0700

    cambio ligero en ProfileCard

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+function ProfileCard ({ title, handle, image}) {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by">
+          <image src={image} alt="" width={200} height={200}/>
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">Title is {title}</p>
+          <p className="subtitle is-6">Handle is {handle}</p>
+        </div>
+      </div>
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    </div>
+  )
+}
+export default ProfileCard
